bf15be39f8234d1c37942cb71a59385ff8a2c6d6
ccbj <2958457028@qq.com>
2026-04-21 15:20:45 +0800

    try

---
+++ tryVue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- html -->
+   <div class="box">
+      ciallo
+   </div>
+</template>
+
+<script>
+  // js/ts
+  export default {
+    name: 'App',//组件名称
+  }
+</script>
+
+<style>
+  /* css */
+  .box {
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+</style>
---
+++ tryVue/src/main.ts
@@ -0,0 +1,6 @@
+// 引入vue
+import { createApp } from "vue"
+// 引入app组件
+import App from "./App.vue"
+// 创建应用实例并挂载到#app元素上
+createApp(App).mount("#app")

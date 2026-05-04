b0639a75877a04505f9b60dc99df96092b3b0852
ccbj <2958457028@qq.com>
2026-05-04 14:59:01 +0800

    setup语法糖

--- vue3/vue3-demo/src/App.vue
+++ vue3/vue3-demo/src/App.vue
@@ -1,11 +1,24 @@
-<script setup></script>
+<!-- 加上setup允许script直接使用组合api -->
+<script setup>
+  import { onBeforeMount, onMounted,ref } from 'vue'
+
+  // 这里可以直接使用组合api
+  console.log('setup')
+
+  onBeforeMount(() => {
+    console.log('onBeforeMount');
+  })
+
+  let msg = ref('ciallo')
+  const count = ref(0)
+  const ciallo = () => {
+    console.log(`ciallo ${count.value++}`)
+    msg.value = `ciallo ${count.value}`
+  }
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ msg }}</h1>
+  <button @click="ciallo">+ 1</button>
 </template>
-
-<style scoped></style>

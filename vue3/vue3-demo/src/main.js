9c982b5dcb6328a01030fbfaaedb4b0c50b4dce5
ccbj <2958457028@qq.com>
2026-05-05 12:48:36 +0800

    生命周期api

--- vue3/vue3-demo/src/main.js
+++ vue3/vue3-demo/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 // import Active from './reactive.vue'
 // import Computed from './computed.vue'
-import Watch from './watch.vue'
+// import Watch from './watch.vue'
+import Api from './生命周期api.vue'
 
 
 
 // createApp(App).mount('#app')
 // createApp(Active).mount('#app')
 // createApp(Computed).mount('#app')
-createApp(Watch).mount('#app')
+// createApp(Watch).mount('#app')
+createApp(Api).mount('#app')
---
+++ vue3/vue3-demo/src/生命周期api.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+// beforecreate和created生命周期api放在setup中执行
+const getlist = () => {
+  setTimeout(() => {
+    console.log('getlist')
+  }, 1000)
+}
+getlist()
+// 可以有多个onMounted回调函数
+onMounted(() => {
+  console.log('onMounted--1');
+})
+onMounted(() => {
+  console.log('onMounted--2');
+})
+
+
+
+</script>
+
+<template>
+
+</template>

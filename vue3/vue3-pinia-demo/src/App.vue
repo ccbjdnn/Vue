b44a2ec709d91f0c0433e653e445386726002ccd
ccbj <2958457028@qq.com>
2026-05-06 12:44:56 +0800

    仓库解构和本地小后端测试

--- vue3/vue3-pinia-demo/src/App.vue
+++ vue3/vue3-pinia-demo/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { storeToRefs } from 'pinia'
 import Son1 from '@/components/son1.vue'
 import Son2 from '@/components/son2.vue'
 // 引入channelStore仓库
@@ -8,10 +9,14 @@ const channelStore = useChannelStore()
 import { useCounterStore } from '@/store/counter'
 const counterStore = useCounterStore()
 // console.log(counterStore.count)
+// const { count, doubleCount } = counterStore---不能直接解,会丢失响应式数据
+const { count } = storeToRefs(counterStore)
+const { doubleCount } = counterStore//方法转响应式会失效
+
 </script>
 
 <template>
-  <h1>App.vue根组件 - {{ counterStore.count }} - <button @click="counterStore.doubleCount">&nbsp;翻倍&nbsp;</button></h1>
+  <h1>App.vue根组件 - {{ count }} - <button @click="doubleCount">&nbsp;翻倍&nbsp;</button></h1>
   <son1></son1>
   <son2></son2>
   <hr>
--- vue3/vue3-pinia-demo/src/store/channel.js
+++ vue3/vue3-pinia-demo/src/store/channel.js
@@ -17,4 +17,15 @@ export const useChannelStore = defineStore('channel',()=>{
     getChannelList
   }
 })
+// 本地后端测试
+// axios.get('http://localhost:3000/api/user')
+//   .then(res => {
+//     console.log(res.data)
+//   })
 
+// axios.post('http://localhost:3000/api/login', {
+//   username: 'admin',
+//   password: '123456'
+// }).then(res => {
+//   console.log(res.data)
+// })

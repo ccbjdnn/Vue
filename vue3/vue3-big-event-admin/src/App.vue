23efb79e1d56f9b2b8ca988d4f28c2723ea55fc9
ccbj <2958457028@qq.com>
2026-05-07 17:18:53 +0800

    搭建路由

--- vue3/vue3-big-event-admin/src/App.vue
+++ vue3/vue3-big-event-admin/src/App.vue
@@ -4,6 +4,11 @@
 </script>
 
 <template>
+<hr>
+<hr>
+<router-view></router-view>
+<hr>
+<hr>
   <h1>{{ userStore.token }}</h1>
   <el-button @click="userStore.setToken('123456')">login</el-button>
   <el-button @click="userStore.removeToken()">logout</el-button>
---
+++ vue3/vue3-big-event-admin/src/views/user/UserPassword.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    用户密码
+  </div>
+</template>

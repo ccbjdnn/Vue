afb5e19346872f344d79de9ebe9b26e318e1e02a
ccbj <2958457028@qq.com>
2026-05-07 17:54:13 +0800

    login

--- vue3/vue3-big-event-admin/src/App.vue
+++ vue3/vue3-big-event-admin/src/App.vue
@@ -1,18 +1,8 @@
 <script setup>
-  import { useUserStore } from '@/stores'
-  const userStore = useUserStore()
 </script>
 
 <template>
-<hr>
-<hr>
 <router-view></router-view>
-<hr>
-<hr>
-  <h1>{{ userStore.token }}</h1>
-  <el-button @click="userStore.setToken('123456')">login</el-button>
-  <el-button @click="userStore.removeToken()">logout</el-button>
-  <exam></exam>
 </template>
 
 <style scoped></style>
